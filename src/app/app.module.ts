5e565f705ddd336853ec60a8be60366e2b68d406
atonho <atonho.vil@gmail.com>
2019-10-09 22:37:28 -0300

    incluido pasta "quarta'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoiteComponent } from './noite/noite.component';
+import { QuartaComponent } from './quarta/quarta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoiteComponent
+    NoiteComponent,
+    QuartaComponent
   ],
   imports: [
     BrowserModule,
